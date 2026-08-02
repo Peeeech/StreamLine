9142fe1bea45bcceddb785b6cda76e6b1f7b5b82
Peeeech <159792426+Peeeech@users.noreply.github.com>
2026-08-02 15:49:43 -0500

    Delete io_scene_pmmap/materials/namepars.tpl.py

--- io_scene_pmmap/materials/namepars.tpl.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-import re
-
-if __name__ == "__main__":
-    files = os.listdir(sys.argv[1])
-    for file in files:
-        index = (re.split(r"_", file))[1]
-        print(index)
